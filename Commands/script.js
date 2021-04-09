739d05c91641fa1a3880ab2fef32a5ed92bb1add
Bence Tuzson <tuzsonbence2003@gmail.com>
2021-04-09 02:15:11 +0200

    script subcommands added and parms modified

--- Commands/script.js
+++ Commands/script.js
@@ -6,54 +6,56 @@ module.exports = {
     description: 'DMs a specific script or the message\'s author\'s lines',
     async execute(interaction, args, database, bot) {
         let script;
-        if (!args[2].value) {
-            try {
-                script = fs.readFileSync(`../src/scripts/s${args[0].value}e${args[1].value}.txt`, 'utf8');
-            } catch (e) {
-                bot.api.interactions(interaction.id, interaction.token).callback.post({data: { type: 4, data: {
-                    content: "Érvénytelen paraméter!"
-                }}});
-                return;
-            }
-
-            const splitScript = script.split('\n');
-            let splitLine;
-            let scriptString = "";
-            let lineCount = 0;
-            const nickname = bot.guilds.cache.get(interaction.guild_id).members.cache.get(interaction.member.user.id).nickname;
-            let temp = "";
-            const Embed = new Discord.MessageEmbed();
-
-            function line(i) {
-                return `**${nickname !== splitLine[0] ? `${i + 1} [${splitLine[0].slice(splitLine[0].indexOf('(') + 1, splitLine[0].lastIndexOf(')'))}] ` : i + 1}:** ${replaceAll(onlyScript(splitLine), "*", "\\*")}\n`
-            }
-
-            for (let i = 0; i < splitScript.length; ++i) {
-                splitLine = splitScript[i].split(": ");
-                if (splitLine[0].replace(splitLine[0].slice(splitLine[0].indexOf('(') - 1, splitLine[0].lastIndexOf(')') + 1), "") === nickname) {
-                    temp = scriptString;
-                    scriptString += line(i);
-                    if (scriptString.length > 1024) {
-                        Embed.addField("\u200B", temp);
-                        scriptString = line(i);
-                    }
-                    ++lineCount;
+        switch (args[0].name) {
+            case "lines":
+                try {
+                    script = fs.readFileSync(`../src/scripts/s${args[0].options[0].value}e${args[0].options[1].value}.txt`, 'utf8');
+                } catch (e) {
+                    bot.api.interactions(interaction.id, interaction.token).callback.post({data: { type: 4, data: {
+                        content: "Érvénytelen paraméter!"
+                    }}});
+                    return;
                 }
-            }
-            if (scriptString !== "") Embed
-                .addField("\u200B", scriptString)
-                .setTitle(`Your line${lineCount > 1 ? "s" : ""} for S${args[0].value}E${args[1].value}`)
-                .setColor('RANDOM');
 
-            bot.users.cache.get(interaction.member.user.id).send(scriptString !== "" ? Embed : `You don't have any scripts in S${args[0].value}E${args[1].value}!`);
-        } else {
-            try {
-                bot.users.cache.get(interaction.member.user.id).send(`S${args[0].value}E${args[1].value} script`, {files: [`../src/scripts/full/${database.SCRIPT_FILES[`S${args[0].value}E${args[1].value}`]}`]});
-            } catch (e) {
-                bot.api.interactions(interaction.id, interaction.token).callback.post({data: { type: 4, data: {
-                    content: "Érvénytelen paraméter!"
-                }}});
-            }
+                const splitScript = script.split('\n');
+                let splitLine;
+                let scriptString = "";
+                let lineCount = 0;
+                const nickname = args[0].options[2] ? args[0].options[2].value : bot.guilds.cache.get(interaction.guild_id).members.cache.get(interaction.member.user.id).nickname;
+                let temp = "";
+                const Embed = new Discord.MessageEmbed();
+
+                function line(i) {
+                    return `**${nickname !== splitLine[0] ? `${i + 1} [${splitLine[0].slice(splitLine[0].indexOf('(') + 1, splitLine[0].lastIndexOf(')'))}] ` : i + 1}:** ${replaceAll(onlyScript(splitLine), "*", "\\*")}\n`
+                }
+
+                for (let i = 0; i < splitScript.length; ++i) {
+                    splitLine = splitScript[i].split(": ");
+                    if (splitLine[0].replace(splitLine[0].slice(splitLine[0].indexOf('(') - 1, splitLine[0].lastIndexOf(')') + 1), "") === nickname) {
+                        temp = scriptString;
+                        scriptString += line(i);
+                        if (scriptString.length > 1024) {
+                            Embed.addField("\u200B", temp);
+                            scriptString = line(i);
+                        }
+                        ++lineCount;
+                    }
+                }
+                if (scriptString !== "") Embed
+                    .addField("\u200B", scriptString)
+                    .setTitle(`${args[0].options[2] ? `${nickname}'s` : "Your"} line${lineCount > 1 ? "s" : ""} for S${args[0].options[0].value}E${args[0].options[1].value}`)
+                    .setColor('RANDOM');
+
+                bot.users.cache.get(interaction.member.user.id).send(scriptString !== "" ? Embed : `You don't have any scripts in S${args[0].options[0].value}E${args[0].options[1].value}!`);
+                break;
+            case "full":
+                try {
+                    bot.users.cache.get(interaction.member.user.id).send(`S${args[0].options[0].value}E${args[0].options[1].value} script`, {files: [`../src/scripts/full/${database.SCRIPT_FILES[`S${args[0].options[0].value}E${args[0].options[1].value}`]}`]});
+                } catch (e) {
+                    bot.api.interactions(interaction.id, interaction.token).callback.post({data: { type: 4, data: {
+                        content: "Érvénytelen paraméter!"
+                    }}});
+                }
         }
 
         bot.api.interactions(interaction.id, interaction.token).callback.post({data: { type: 4, data: {
